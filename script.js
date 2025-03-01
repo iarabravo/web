7b55172a1972115bdaf2a8ba89b344760d500f36
iarabravo <iarabravo2017@hotmail.com>
2025-03-01 13:31:56 -0300

    Se ha generado cambios

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const close = document.getElementById('close');
+var MainImg = document.getElementById("MainImg");
+var smallImg = document.getElementsByClassName('small-img');
 
 if (bar){
     bar.addEventListener('click',()=>{
@@ -29,3 +31,16 @@ document.addEventListener("DOMContentLoaded", function () {
         dropdowns.forEach(dropdown => dropdown.classList.remove("active"));
     });
 });
+
+smallImg[0].onclick=function(){
+    MainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick=function(){
+    MainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick=function(){
+    MainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick=function(){
+    MainImg.src = smallImg[3].src;
+}
